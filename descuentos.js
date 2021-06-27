21544286986920980d30a381eef3b0ce47a6f197
fermin <fermartorr01@gmail.com>
2021-06-27 13:24:43 +0200

    Hago el taller de calcular los descuentos de un precio base y escribo texto html desde javaScript

--- descuentos.js
+++ descuentos.js
@@ -8,6 +8,22 @@ function calcularPrecioConDescuento(precio, descuento) {
   return precioConDescuento;
 }
 
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento es de " + precioConDescuento + "$";
+}
+
+
+
+
 
 // console.log({
 //   precioOriginal,
