0571caf8c383fb3c240113001a0548c205ba95bb
fermin <fermartorr01@gmail.com>
2021-07-04 12:34:05 +0200

    HSacamos la media ponderada como ejercicio del profe en un archivo escrito

---
+++ promedioPonderado.js
@@ -0,0 +1,21 @@
+const notes = [
+  {
+      course: "Educación Física",
+      note: 10,
+      credit: 2,
+  },
+  {
+      course: "Programación",
+      note: 8,
+      credit: 5,
+  },
+  {
+      course: "Finanzas personales",
+      note: 7,
+      credit: 5,
+  },
+];
+
+const notesWithCredit = notes.map(function(noteObject){
+  return noteObject.note * noteObject.credit;
+});
